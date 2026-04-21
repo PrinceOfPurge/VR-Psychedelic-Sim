710adb77c70c107e23c7de49d73a628a746bb24c
IzzyDev <kishoreishaan@gmail.com>
2026-04-21 17:22:05 -0400

    Set up base transition, will use Gemini for the advanced timing script

--- Assets/Scripts/Izzy/StartMedicine.cs
+++ Assets/Scripts/Izzy/StartMedicine.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Serialization;
 
 [Serializable]
 struct DistortedUVsInfoContainer
@@ -9,26 +10,84 @@ struct DistortedUVsInfoContainer
     public string shaderEffectParamName;
 }
 
+
 public class StartMedicine : MonoBehaviour
 {
-    [Header("Materials")] 
-    [SerializeField] private DistortedUVsInfoContainer[] envPsy;
-    
-    [Header("Volumes")]
+    [FormerlySerializedAs("envPsy"),Header("References")] 
+    [SerializeField] private DistortedUVsInfoContainer[] environmentalPsychedelicMaterials;
     [SerializeField] private Volume descentVolume;
+
+    [Header("Parameters")]
+    [SerializeField] private float transitionDuration = 15f;
+
+    private bool transitioning;
+    private float elapsedTransitionTime;
+
+    private void Start()
+    {
+        ResetEffects();
+    }
 
     private void Update()
     {
+        // Start transition
         if (Input.GetKeyDown(KeyCode.F))
-        {
-            // Make environment trippy
-            foreach (DistortedUVsInfoContainer matInfo in envPsy)
-            {
-                matInfo.mat.SetFloat(matInfo.shaderEffectParamName, 1f);
-            }
+            transitioning = true;
+        
+        if (transitioning)
+            StartMedicineEffects();
+        else
+            elapsedTransitionTime = 0f;
+
+        // Reset effects
+        if (Input.GetKeyDown(KeyCode.G))
+            ResetEffects();
+    }
+
+    private void StartMedicineEffects()
+    {
+        // Transition
+        elapsedTransitionTime += Time.deltaTime;
+        float t = elapsedTransitionTime / transitionDuration;
             
-            // Blend in trippy volume
-            descentVolume.weight = 1f;
+        // Make environment trippy
+        if (environmentalPsychedelicMaterials.Length > 0)
+        {
+            foreach (DistortedUVsInfoContainer matInfo in environmentalPsychedelicMaterials)
+            {
+                matInfo.mat.SetFloat(matInfo.shaderEffectParamName, t);
+            }
         }
+            
+        // Blend in trippy volume
+        if (descentVolume != null) descentVolume.weight = t;
+    }
+
+    private void ResetEffects()
+    {
+        // Reset transition
+        transitioning = false;
+
+        // Make environment normal
+        if (environmentalPsychedelicMaterials.Length > 0)
+        {
+            foreach (DistortedUVsInfoContainer matInfo in environmentalPsychedelicMaterials)
+            {
+                matInfo.mat.SetFloat(matInfo.shaderEffectParamName, 0f);
+            }
+        }
+
+        // Remove trippy volume
+        if (descentVolume != null) descentVolume.weight = 0f;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        StartMedicineEffects();
+    }
+
+    private void OnApplicationQuit()
+    {
+        ResetEffects();
     }
 }
